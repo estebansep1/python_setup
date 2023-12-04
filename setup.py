f0433841ae75b10ae42eeca99267b632e3ad5822
Esteban Sepulveda <tebansd@icloud.com>
2023-12-04 13:32:11 -0800

    final solution

--- setup.py
+++ setup.py
@@ -1,54 +1,59 @@
-#1. arb_args - Takes in any number of arguments and prints them one at a time.
-def arb_args(*args):
-    for i in args:
-        print(i)
+# 1. Write a Python function called max_num()to find the maximum of three numbers.
+def max_num(a, b, c):
+    return max([a, b, c])
 
-arb_args(1, 6, 7, 2, 7)        
-
-#2. inner_func - Takes in two integers. Two nested functions should perform separate, distinct math operations using the integers. The result of both nested functions should then be added together and printed.
-def inner_func(x, y):
-    def inner_1():
-        return x + y
-    def inner_2():
-        return x - y
-    print(inner_1() + inner_2())
-
-inner_func(8, 24)
-
-#3.lunch_lady - Takes in two strings: a student's name and their lunch preference. The function should print both strings. If a lunch preference is not given, "Mystery Meat" should be printed instead.
-def lunch_lady(name, lunch="Mystery Meat"):
-    print(name, lunch)
-
-lunch_lady('Esteban')
-
-#4. sum_n_product - Accepts two integers and returns both the sum and the product.
-def sum_n_product(x, y):
-    return x + y, x * y
-
-print(sum_n_product(8, 24))
-
-#5. alias_arb_args - Should be arb_args but assigned an alias. Remember, variables can hold functions in Python just like they can in JS. Alternatively, you can call a function from inside another function.
+print(max_num(8, 24, 13))
 
 
-#6. arb_mean — Accepts any number of integers and prints their average
+# 2. Write a Python function called mult_list() to multiply all the numbers in a list.
+def mult_list(numbers):
+    result = 1
+    for num in numbers:
+        result *= num
+    return result
 
-def arb_mean(*args):
-    total = 0
-    for a in args:
-        total += a
-    print(a/len(args))
+print(mult_list([3, 3, 3]))
 
-arb_mean(32)
 
-#7. arb_longest_string — Accepts any number of strings and returns the longest one.
-def arb_longest_string(*args):
-    long = 0
-    longest = ''
-    for a in args:
-        if len(a) > long:
-            long = len(a)
-            longest = a
-    return longest
+# 3. Write a Python function called rev_string() to reverse a string.
+def rev_string(str):
+    return str[::-1]
 
-result = arb_longest_string('Which', 'one', 'of', 'these', 'strings', 'is', 'the', 'longest', '?')
-print(result)
+print(rev_string('Kawhi Leonard'))
+
+
+# 4. Write a Python function called num_within() to check whether a number falls in a given range.
+def num_within(x,a,b):
+  return x in range(a,b+1)
+     
+print(num_within(3,1,3))   
+print(num_within(10,2,5))   
+
+
+# 5. Write a Python function called pascal() that prints out the first n rows of Pascal's triangle.
+triangle = [[1],[1,1]]
+def pascal(n):
+  if n < 1:
+    print("invalid number of rows")
+  elif n == 1:
+    print(triangle[0])
+  else:
+    row_number = 2
+    while len(triangle) < n:
+      row = []
+      row_prev = triangle[row_number - 1]
+      length = len(row_prev)+1
+      for i in range(length):
+        if i == 0:
+          row.append(1)
+        elif i > 0 and i < length-1:
+          row.append(triangle[row_number-1][i-1]+triangle[row_number-1][i])
+        else:
+          row.append(1)
+      triangle.append(row)
+      row_number += 1
+
+    for row in triangle:
+      print(row)
+
+pascal(5)
